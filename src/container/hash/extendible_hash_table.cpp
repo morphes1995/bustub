b95a2f8db65c980a939f093665c0efcdc0614137
morphes1995 <morphes1995@gmail.com>
2024-08-18 18:11:11 +0800

    implement extendable hash table
    Signed-off-by: morphes1995 <morphes1995@gmail.com>

--- src/container/hash/extendible_hash_table.cpp
+++ src/container/hash/extendible_hash_table.cpp
@@ -23,7 +23,9 @@ namespace bustub {
 
 template <typename K, typename V>
 ExtendibleHashTable<K, V>::ExtendibleHashTable(size_t bucket_size)
-    : global_depth_(0), bucket_size_(bucket_size), num_buckets_(1) {}
+    : global_depth_(0), bucket_size_(bucket_size), num_buckets_(1) {
+  this->dir_.push_back(std::make_shared<Bucket>(bucket_size));
+}
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::IndexOf(const K &key) -> size_t {
@@ -66,17 +68,57 @@ auto ExtendibleHashTable<K, V>::GetNumBucketsInternal() const -> int {
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Find(const K &key, V &value) -> bool {
-  UNREACHABLE("not implemented");
+  std::scoped_lock lock(latch_);
+
+  size_t i = IndexOf(key);
+  std::shared_ptr<Bucket> bucket = this->dir_[i];
+  return bucket->Find(key, value);
 }
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Remove(const K &key) -> bool {
-  UNREACHABLE("not implemented");
+  std::scoped_lock lock(latch_);
+  return this->dir_[IndexOf(key)]->Remove(key);
 }
 
 template <typename K, typename V>
 void ExtendibleHashTable<K, V>::Insert(const K &key, const V &value) {
-  UNREACHABLE("not implemented");
+  std::scoped_lock lock(latch_);
+
+  size_t idx = IndexOf(key);
+  std::shared_ptr<Bucket> bucket = this->dir_[idx];
+
+  while (bucket->IsFull()) {
+    // double the slot array;
+    if (bucket->GetDepth() == this->global_depth_) {
+      int num_buckets = 1 << this->global_depth_;
+      for (int i = 0; i < num_buckets; i++) {
+        this->dir_.push_back(this->dir_[i]);
+      }
+      this->global_depth_ += 1;
+    }
+
+    // create two new buckets
+    int mask = 1 << bucket->GetDepth();
+    int idx2 =
+        (mask & idx) == 0 ? mask + idx : idx - mask;  // corresponding bucket index to offload KVs in idx-th bucket
+    this->dir_[idx] = std::make_shared<Bucket>(bucket_size_, bucket->GetDepth() + 1);
+    this->dir_[idx2] = std::make_shared<Bucket>(bucket_size_, bucket->GetDepth() + 1);
+    this->num_buckets_ += 1;
+
+    // rehash kv to new buckets
+    std::for_each(bucket->GetItems().begin(), bucket->GetItems().end(), [this](const auto &item) {
+      size_t new_idx = this->IndexOf(item.first);
+      this->dir_[new_idx]->Insert(item.first, item.second);
+    });
+
+    // update target bucket for next loop (target bucket may be still full)
+    idx = IndexOf(key);
+    bucket = this->dir_[idx];
+  }
+
+  // bucket is not full now , inset and return
+  bucket->Insert(key, value);
 }
 
 //===--------------------------------------------------------------------===//
@@ -87,17 +129,66 @@ ExtendibleHashTable<K, V>::Bucket::Bucket(size_t array_size, int depth) : size_(
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Bucket::Find(const K &key, V &value) -> bool {
-  UNREACHABLE("not implemented");
+  return std::any_of(this->list_.begin(), this->list_.end(), [&key, &value](const auto &it) {
+    if (it.first == key) {
+      value = it.second;
+      return true;
+    }
+    return false;
+  });
+
+  //  auto  it = this->list_.begin();
+  //  auto end = this->list_.end();
+  //  while (it != end){
+  //    if (it->first == key){
+  //      value = it->second;
+  //      return true;
+  //    }
+  //    ++it;
+  //  }
+  //  return false;
 }
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Bucket::Remove(const K &key) -> bool {
-  UNREACHABLE("not implemented");
+  return std::any_of(this->list_.begin(), this->list_.end(), [&key, this](const auto &it) {
+    if (it.first == key) {
+      this->list_.remove(it);
+      return true;
+    }
+    return false;
+  });
+
+  //    auto  it = this->list_.begin();
+  //    auto end = this->list_.end();
+  //    while (it != end){
+  //      if (it->first == key){
+  //        this->list_.remove(*it);
+  //        return true;
+  //      }
+  //      ++it;
+  //    }
+  //    return false;
 }
 
 template <typename K, typename V>
 auto ExtendibleHashTable<K, V>::Bucket::Insert(const K &key, const V &value) -> bool {
-  UNREACHABLE("not implemented");
+  if (this->IsFull()) {
+    return false;
+  }
+
+  auto it = this->list_.begin();
+  auto end = this->list_.end();
+  while (it != end) {
+    if (it->first == key) {
+      it->second = value;
+      return true;
+    }
+    ++it;
+  }
+
+  this->list_.emplace_back(key, value);
+  return true;
 }
 
 template class ExtendibleHashTable<page_id_t, Page *>;
--- test/primer/starter_cast_test.cpp
+++ test/primer/starter_cast_test.cpp
@@ -1,15 +1,16 @@
 #include <bitset>
-#include <thread>  // NOLINT
 #include <exception>
 #include <iostream>
+#include <thread>  // NOLINT
 #include "gtest/gtest.h"
 
 using namespace std;
-class Complex{
+class Complex {
  public:
-  Complex(double real = 0.0, double imag = 0.0): m_real(real), m_imag(imag){ }
+  Complex(double real = 0.0, double imag = 0.0) : m_real(real), m_imag(imag) {}
+
  public:
-  operator double() const { return m_real; } //类型转换函数
+  operator double() const { return m_real; }  //类型转换函数
  private:
   double m_real;
   __attribute__((unused)) double m_imag;
@@ -17,9 +18,9 @@ class Complex{
 
 TEST(StarterCastTest, StaticCastTest) {
   ////  //下面是正确的用法
-//    int m = 100;
-//    Complex c(12.5, 23.8);
-//    long n = static_cast<long>(m); //宽转换，没有信息丢失
+  //    int m = 100;
+  //    Complex c(12.5, 23.8);
+  //    long n = static_cast<long>(m); //宽转换，没有信息丢失
   //  char ch = static_cast<char>(m); //窄转换，可能会丢失信息
   //  int *p1 = static_cast<int*>( malloc(10 * sizeof(int)) ); //将void指针转换为具体类型指针
   //  void *p2 = static_cast<void*>(p1); //将具体类型指针，转换为void指针
@@ -29,130 +30,123 @@ TEST(StarterCastTest, StaticCastTest) {
   //  p3 = static_cast<float*>(0X2DF9); //不能将整数转换为指针类型
   //  int *i = nullptr;
   //  long *l = static_cast<long*>(i); // Static_cast from 'int *' to 'long *' is not allowed
-
 }
 
 TEST(StarterCastTest, ConstCastTest) {
   int a = 12;
   const int n = a;
-  int *p = const_cast<int*>(&n);
+  int *p = const_cast<int *>(&n);
   *p = 234;
-  ASSERT_EQ(n ,234);
+  ASSERT_EQ(n, 234);
   ASSERT_EQ(*p, 234);
 
-
   const int n2 = 12;
-  int *p2 = const_cast<int*>(&n); // C++ 对常量的处理更像是编译时期的#define，是一个值替换的过程
+  int *p2 = const_cast<int *>(&n);  // C++ 对常量的处理更像是编译时期的#define，是一个值替换的过程
   *p2 = 234;
-  ASSERT_EQ(n2 ,12); // 代码中所有使用 n2 的地方在编译期间就被替换成了 100
+  ASSERT_EQ(n2, 12);  // 代码中所有使用 n2 的地方在编译期间就被替换成了 100
   ASSERT_EQ(*p2, 234);
 }
 
-class MyBase
-{
+class MyBase {
  public:
   virtual void test() {}
 };
 class MyChild : public MyBase {};
 TEST(StarterCastTest, ConstDynamicTest) {
-    // dynamic cast
-    MyBase  *base = new MyBase();
-    MyChild *child_p = dynamic_cast<MyChild*>(base);
-    if (child_p == nullptr)
-      std::cout << "Null pointer returned" << std::endl;
-    try
-    {
-      __attribute__((unused)) MyChild &child = dynamic_cast<MyChild&>(*base); // bad dynamic_cast
-    }
-    catch(std::bad_cast &e)
-    {
-      std::cout << e.what() << std::endl; // bad dynamic_cast
-    }
+  // dynamic cast
+  MyBase *base = new MyBase();
+  MyChild *child_p = dynamic_cast<MyChild *>(base);
+  if (child_p == nullptr) std::cout << "Null pointer returned" << std::endl;
+  try {
+    __attribute__((unused)) MyChild &child = dynamic_cast<MyChild &>(*base);  // bad dynamic_cast
+  } catch (std::bad_cast &e) {
+    std::cout << e.what() << std::endl;  // bad dynamic_cast
+  }
 
-    MyChild a;
-    MyBase &b = a; // Base ref to Child
-    __attribute__((unused)) MyChild &c = dynamic_cast<MyChild&>(b); // good ref cast
+  MyChild a;
+  MyBase &b = a;                                                    // Base ref to Child
+  __attribute__((unused)) MyChild &c = dynamic_cast<MyChild &>(b);  // good ref cast
 
-    MyBase a2;
-    MyBase &b2 = a2;
-    try
-    {
-      __attribute__((unused)) MyChild &c2 = dynamic_cast<MyChild&>(b2); // bad ref dynamic_cast
-    }
-    catch(std::bad_cast &e)
-    {
-      std::cout << e.what() << std::endl; // bad dynamic_cast
-    }
-
+  MyBase a2;
+  MyBase &b2 = a2;
+  try {
+    __attribute__((unused)) MyChild &c2 = dynamic_cast<MyChild &>(b2);  // bad ref dynamic_cast
+  } catch (std::bad_cast &e) {
+    std::cout << e.what() << std::endl;  // bad dynamic_cast
+  }
 }
-class A{
-   public:
-    virtual void func() const { cout<<"Class A"<<endl; }
-   private:
-    __attribute__((unused)) int m_a;
+class A {
+ public:
+  virtual void func() const { cout << "Class A" << endl; }
+
+ private:
+  __attribute__((unused)) int m_a;
 };
-class B: public A{
-   public:
-    virtual void func() const { cout<<"Class B"<<endl; }
-   private:
-    __attribute__((unused)) int m_b;
+class B : public A {
+ public:
+  virtual void func() const { cout << "Class B" << endl; }
+
+ private:
+  __attribute__((unused)) int m_b;
 };
-class C: public B{
-   public:
-    virtual void func() const { cout<<"Class C"<<endl; }
-   private:
-    __attribute__((unused)) int m_c;
+class C : public B {
+ public:
+  virtual void func() const { cout << "Class C" << endl; }
+
+ private:
+  __attribute__((unused)) int m_c;
 };
-class D: public C{
-   public:
-    virtual void func() const { cout<<"Class D"<<endl; }
-   private:
-    __attribute__((unused)) int m_d;
+class D : public C {
+ public:
+  virtual void func() const { cout << "Class D" << endl; }
+
+ private:
+  __attribute__((unused)) int m_d;
 };
 
-TEST(StarterCastTest, DynamicCastTest){
-      A *pa = new A();
-      B *pb;
-      C *pc;
-      D *pd;
-      //情况①
-      pb = dynamic_cast<B*>(pa); //向下转型失败
-      if(pb == NULL){
-        cout<<"Downcasting failed: A* to B*"<<endl;
-      }else{
-        cout<<"Downcasting successfully: A* to B*"<<endl;
-        pb -> func();
-      }
-      pc = dynamic_cast<C*>(pa); //向下转型失败
-      if(pc == NULL){
-        cout<<"Downcasting failed: A* to C*"<<endl;
-      }else{
-        cout<<"Downcasting successfully: A* to C*"<<endl;
-        pc -> func();
-      }
-      cout<<"-------------------------"<<endl;
-      //情况②
-      pa = new D(); //向上转型都是允许的
-      pb = dynamic_cast<B*>(pa); //向下转型成功
-      if(pb == NULL){
-        cout<<"Downcasting failed: A* to B*"<<endl;
-      }else{
-        cout<<"Downcasting successfully: A* to B*"<<endl;
-        pb -> func();
-      }
-      pc = dynamic_cast<C*>(pa); //向下转型成功
-      if(pc == NULL){
-        cout<<"Downcasting failed: A* to C*"<<endl;
-      }else{
-        cout<<"Downcasting successfully: A* to C*"<<endl;
-        pc -> func();
-      }
+TEST(StarterCastTest, DynamicCastTest) {
+  A *pa = new A();
+  B *pb;
+  C *pc;
+  D *pd;
+  //情况①
+  pb = dynamic_cast<B *>(pa);  //向下转型失败
+  if (pb == NULL) {
+    cout << "Downcasting failed: A* to B*" << endl;
+  } else {
+    cout << "Downcasting successfully: A* to B*" << endl;
+    pb->func();
+  }
+  pc = dynamic_cast<C *>(pa);  //向下转型失败
+  if (pc == NULL) {
+    cout << "Downcasting failed: A* to C*" << endl;
+  } else {
+    cout << "Downcasting successfully: A* to C*" << endl;
+    pc->func();
+  }
+  cout << "-------------------------" << endl;
+  //情况②
+  pa = new D();                //向上转型都是允许的
+  pb = dynamic_cast<B *>(pa);  //向下转型成功
+  if (pb == NULL) {
+    cout << "Downcasting failed: A* to B*" << endl;
+  } else {
+    cout << "Downcasting successfully: A* to B*" << endl;
+    pb->func();
+  }
+  pc = dynamic_cast<C *>(pa);  //向下转型成功
+  if (pc == NULL) {
+    cout << "Downcasting failed: A* to C*" << endl;
+  } else {
+    cout << "Downcasting successfully: A* to C*" << endl;
+    pc->func();
+  }
 
-      pd = dynamic_cast<D*>(pa); //向下转型成功
-      if(pd == NULL){
-        cout<<"Downcasting failed: A* to D*"<<endl;
-      }else{
-        cout<<"Downcasting successfully: A* to D*"<<endl;
-        pd -> func();
-      }
+  pd = dynamic_cast<D *>(pa);  //向下转型成功
+  if (pd == NULL) {
+    cout << "Downcasting failed: A* to D*" << endl;
+  } else {
+    cout << "Downcasting successfully: A* to D*" << endl;
+    pd->func();
+  }
 }
